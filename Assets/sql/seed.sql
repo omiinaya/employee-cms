32bd98dd0c8e4851a718f875e1cb6cb9951e9187
omiinaya <omiinaya@gmail.com>
2020-03-28 07:35:47 -0400

    updated manager id title on db seed.

--- Assets/sql/seed.sql
+++ Assets/sql/seed.sql
@@ -11,7 +11,7 @@ VALUES
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
+    ('Manager', 100000, 1),
     ('Salesperson', 80000, 1),
     ('Lead Engineer', 150000, 2),
     ('Software Engineer', 120000, 2),
